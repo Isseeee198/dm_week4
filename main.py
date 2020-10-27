1167e57d70300dd86a1a4ed7cdc0d6073a82afbc
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2020-10-27 17:42:26 +0900

    a

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ X, Y = datasets.load_linear_example1()
 
 import regression
 model = regression.LinearRegression()
+
+import importlib
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X,Y)
+model.theta
+print(model.theta)
