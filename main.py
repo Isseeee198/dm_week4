3ac557035b8495d12f0df1d0789e949c615f294a
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2020-11-10 17:51:52 +0900

    ver5

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import datasets2
 X, Y = datasets2.load_nonlinear_example1()
 #ex_X = datasets2.polynomial2_features(X)
 ex_X = datasets2.polynomial3_features(X)
-print(ex_X)
-print(Y)
+#print(ex_X)
+#print(Y)
 
 
 import regression
@@ -14,9 +14,14 @@ model = regression.LinearRegression()
 
 import importlib
 importlib.reload(regression)
-model = regression.LinearRegression()
-model.fit(X,Y)
+#model = regression.LinearRegression()
+model = regression.RidgeRegression(alpha=0.1)
+#model.alpha
+model.fit(ex_X,Y)
+print(model.theta)
+
+#model.fit(X,Y)
 #model.theta
 #model.predict(X)
-model.score(X,Y)
+#model.score(X,Y)
 #print(model.score(X,Y))
--- regression.py
+++ regression.py
@@ -17,3 +17,16 @@ class LinearRegression:
   	return (error**2).sum()
   	#pass
   	
+  	
+class RidgeRegression(LinearRegression):
+   alpha = None
+   
+   def __init__(self, alpha=0.1):
+       self.alpha = alpha
+       
+   def fit(self, input, output):
+       xTx = np.dot(input.T, input)
+       I = np.eye(len(xTx))
+       self.theta = np.dot(np.dot(np.linalg.inv(xTx + self.alpha*I),input.T),output)
+       
+       
