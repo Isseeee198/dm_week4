15c3b16a2a3f3f51835d352806f17a0f096cd893
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2020-11-10 17:11:29 +0900

    データ拡張

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import datasets
-X, Y = datasets.load_linear_example1()
+#import datasets
+#X, Y = datasets.load_linear_example1()
+
+import datasets2
+X, Y = datasets2.load_nonlinear_example1()
+ex_X = datasets2.polynomial2_features(X)
+print(ex_X)
+print(Y)
 
 
 import regression
